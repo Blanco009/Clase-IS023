067bb61dc7156692d96acccb826e08f2f6801d24
Blanco <ar96br@gmail.com>
2022-11-21 21:15:25 -0600

    Project finished

--- Trabajo programado 2/Microtrabajo2 final.java
+++ Trabajo programado 2/Microtrabajo2 final.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
-
+//segundo
 public class Microtrabajo2 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
